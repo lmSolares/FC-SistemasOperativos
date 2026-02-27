a77b37d2a40912c3e0bcab43a565d62ef94a8ce1
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-02-26 18:52:31 -0600

    Se implementa la primera parte de la simulación(cargar los procesos y mostrarlos en consola)

---
+++ src/java/simulations/Simulation.java
@@ -0,0 +1,33 @@
+package src.java.simulations;
+
+import src.java.utilities.ReadFile;
+import src.java.entities.Process;
+import java.util.List;
+
+/**
+*   Clase con la implementación de una simulación de técnicas de planificación
+*   de procesos (CPU scheduling) basada en el capítulo de Procesos e Hilos
+*   de Modern Operating Systems, 5th Ed (sección 2.5 Scheduling), se
+*   implementan especificamente los algoritmos: First-Come, First-Served (FCFS)
+*   y Multilevel queues (Múltiples colas por prioridad).
+*/
+public class Simulation{
+
+    // Número máximo de procesos
+    private int MAX_PROCESSES = 200;
+
+    // Método main
+    public void main(){
+
+        // Cargamos los procesos del archivo Procesos.txt
+        List<Process> processes = ReadFile.readProcessFile("files/Procesos.txt", MAX_PROCESSES);
+
+        // Mostramos los procesos cargados
+        for(Process process : processes){
+            System.out.println(process.toString());
+        }
+
+
+    }
+
+}
