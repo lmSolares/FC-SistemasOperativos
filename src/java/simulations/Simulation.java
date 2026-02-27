6a4430e89384ffb158b2b74754e4be9a02d0c623
Luis Ramos <lm.solares@ciencias.unam.mx>
2026-02-27 13:38:03 -0600

    Se implementa la simulación del algoritmo Fair-Share Scheduling (por usuario/grupo)

--- src/java/simulations/Simulation.java
+++ src/java/simulations/Simulation.java
@@ -2,6 +2,7 @@ package src.java.simulations;
 
 import src.java.utilities.ReadFile;
 import src.java.entities.Process;
+import src.java.entities.User;
 import java.util.LinkedList;
 import java.util.ArrayList;
 import java.util.Queue;
@@ -14,7 +15,8 @@ import java.util.Map;
 *   de procesos (CPU scheduling) basada en el capítulo de Procesos e Hilos
 *   de Modern Operating Systems, 5th Ed (sección 2.4 Scheduling), se
 *   implementan especificamente los algoritmos: First-Come, First-Served (FCFS)
-*   y Multilevel queues (Múltiples colas por prioridad).
+*   , Multilevel queues (Múltiples colas por prioridad) y Fair-Share Scheduling
+*   (por usuario/grupo)
 */
 public class Simulation{
 
@@ -97,8 +99,8 @@ public class Simulation{
 
             boolean emptyQueues = readyProcessesQueues.values().stream().allMatch(Queue::isEmpty); // Verificamos si las colas están vacias
             if(processes.isEmpty() && emptyQueues && processInProgress == null){
-                System.out.println("Tiempo de ejecución: " + cpu_timer);
-                System.out.println(processesExecuted.toString());
+                System.out.println("\n\nYa no hay ningun proceso a ejecutar. Tiempo de ejecución: " + cpu_timer);
+                System.out.println("Resumen: " + processesExecuted.toString());
                 return;
             }
 
@@ -106,6 +108,7 @@ public class Simulation{
                 // Agregamos el proceso a la cola que corresponde por su prioridad
                 Process nextProcess = processes.remove(NEXT_PROCESS_THAT_ARRIVES);
                 readyProcessesQueues.get(nextProcess.getPriority()).add(nextProcess);
+                System.out.println("Llego el proceso " + nextProcess.getID() + " time: " + cpu_timer);
             }
 
             // En caso que no haya ningun proceso en ejecución, se toma el siguiente en la cola (tomando en cuenta la prioridad)
@@ -118,27 +121,132 @@ public class Simulation{
                     }
                 }
                 if(processInProgress.getBurst() == processInProgress.getRemaining()){
+                    System.out.println("Primera vez que se ve al proceso " + processInProgress.getID());
                     processInProgress.setStartTime(cpu_timer); // Primer instante en que se ejecuta el proceso
                     processInProgress.setWaitingTime(cpu_timer - processInProgress.getArrival()); // Tiempo de espera del proceso
                     processInProgress.setResponseTime(cpu_timer - processInProgress.getArrival());
                 }else{
+                    System.out.println("Se reanuda la ejecución del proceso " + processInProgress.getID());
                     processInProgress.setWaitingTime(processInProgress.getWaitingTime() + (cpu_timer - (processInProgress.getStartTime() + (processInProgress.getBurst() - processInProgress.getRemaining())))); // Tiempo de espera del proceso
                 }
             }
 
             // Ejecutamos el proceso
             if(processInProgress != null){
+                System.out.println("Se ejecuta el proceso " + processInProgress.getID());
                 processInProgress.executeProcess();
                 processQuantum--; // Reducimos el quantum del proceso
 
                 if(processInProgress.getRemaining() == 0){
+                    System.out.println("El proceso " + processInProgress.getID() + " termino de ejecutarse");
                     processInProgress.setFinishTime(cpu_timer + 1);
                     processInProgress.setTurnaround(processInProgress.getFinishTime() - processInProgress.getArrival());
                     processesExecuted.add(processInProgress);
                     processInProgress = null;
                 }else if(processQuantum == 0) { // En caso de que el quantum del proceso se haya agotado, el proceso se regresa a su cola de prioridad
+                    System.out.println("Se acabo el quantum del proceso " + processInProgress.getID() + " se ingresa nuevamente a la cola de prioridad " + processInProgress.getPriority());
                     readyProcessesQueues.get(processInProgress.getPriority()).add(processInProgress);
                     processInProgress = null;
+                }
+            }
+            cpu_timer++;
+        }
+    }
+
+    /**
+    *   Simulación del algoritmo Fair-Share Scheduling (por usuario), se ejecuta el proceso
+    *   que pertenece al usuario en turno, en caso de el usuario no tener procesos, se cede
+    *   el turno al siguiente usuario, cada usuario tiene un quantum, si se acaba su tiempo
+    *   para ejecutar, el proceso se regreasa la cola de procesos del usuario y el turno se
+    *   asigna al siguiente usuario con procesos en su cola de procesos.
+    *
+    *   @param processes lista de procesos a ejcutar
+    *   @param quantum tiempo que un usuario puede ejecutar
+    *   @param numberOfUsers número de usuarios en la simulación
+    */
+    public void simulateFairShareScheduling(List<Process> processes, int quantum, int numberOfUsers){
+        int NEXT_PROCESS_THAT_ARRIVES = 0;
+        int cpu_timer = 0; // Contador del CPU
+        int userQuantum = 0; // Quantum
+        int userTurn = 1; // Usuario a quien le toca usar la CPU
+
+        Process processInProgress = null;
+        Map<Integer, User> users = new HashMap<>();
+        List<Process> processesExecuted = new ArrayList<>();
+
+        for(int i = 1; i <= numberOfUsers; i++){
+            users.put(i, new User(i));
+        }
+
+        while(true){
+
+            boolean emptyQueues = users.values().stream().allMatch(User::isUserQueueEmpty); // Verificamos si las colas están vacias
+            if(processes.isEmpty() && emptyQueues && processInProgress == null){
+                System.out.println("Tiempo de ejecución: " + cpu_timer);
+                System.out.println(processesExecuted.toString());
+                return;
+            }
+
+            while(!processes.isEmpty() && processes.get(NEXT_PROCESS_THAT_ARRIVES).getArrival() == cpu_timer){ // Se verifica que sigan llegando procesos
+                // Agregamos el proceso a la cola de procesos del usuario al que corresponde
+                Process nextProcess = processes.remove(NEXT_PROCESS_THAT_ARRIVES);
+                users.get(nextProcess.getUser()).addProcessToUser(nextProcess); // Agregamos el proceso a la cola del usuario a la que pertenece
+                System.out.println("Llego el proceso " + nextProcess.getID() + " que pertenece al usuario " + nextProcess.getUser());
+            }
+
+            // Asignamos el turno para ejecutar al usuario que tenga procesos activos
+            if(processInProgress == null){
+                if(userQuantum == 0){
+                    int usersChecked = 0;
+                    while(usersChecked < numberOfUsers){
+                        if(!users.get(userTurn).isUserQueueEmpty()){
+                            processInProgress = users.get(userTurn).pollUserProcess();
+                            userQuantum = quantum;
+                            System.out.println("\nEl usuario " + userTurn + " tiene el proceso " + processInProgress.getID() + " por ejecutar, se le asigna el turno.");
+                            break;
+                        }
+                        userTurn = (userTurn % numberOfUsers) + 1;
+                        usersChecked++;
+                    }
+                } else{
+                    processInProgress = users.get(userTurn).pollUserProcess();
+                }
+                if(processInProgress != null){
+                    if(processInProgress.getBurst() == processInProgress.getRemaining()){
+                        System.out.println("Es la primera vez que se ve al proceso " + processInProgress.getID() + " del usuario " + userTurn);
+                        processInProgress.setStartTime(cpu_timer);
+                        processInProgress.setWaitingTime(cpu_timer - processInProgress.getArrival());
+                        processInProgress.setResponseTime(cpu_timer - processInProgress.getArrival());
+                    } else {
+                        System.out.println("Se reanuda la ejecución del proceso " + processInProgress.getID() + " del usuario " + userTurn);
+                        processInProgress.setWaitingTime(processInProgress.getWaitingTime() + (cpu_timer - (processInProgress.getStartTime() + (processInProgress.getBurst() - processInProgress.getRemaining()))));
+                    }
+                }
+            }
+
+            if(processInProgress != null){
+                System.out.println("Se ejecuta el proceso " + processInProgress.getID() + " del usuario " + userTurn);
+                processInProgress.executeProcess();
+                userQuantum--;
+
+                // Si el proceso termina
+                if(processInProgress.getRemaining() == 0){
+                    System.out.println("El proceso " + processInProgress.getID() + " del usuario " + userTurn + " se termino de ejecutar");
+                    processInProgress.setFinishTime(cpu_timer + 1);
+                    processInProgress.setTurnaround(processInProgress.getFinishTime() - processInProgress.getArrival());
+                    processesExecuted.add(processInProgress);
+
+                    processInProgress = null;
+                    userTurn = (userTurn % numberOfUsers) + 1; // Pasamos el turno al siguiente usuario
+
+                }
+                // Si se acaba el quantum del usuario
+                else if(userQuantum == 0) {
+                    // REGRESAMOS EL PROCESO A SU DUEÑO (sin importar el userTurn)
+                    users.get(processInProgress.getUser()).addProcessToUser(processInProgress);
+
+                    processInProgress = null;
+                    userTurn = (userTurn % numberOfUsers) + 1; // Pasamos el turno al siguiente usuario
                 }
             }
             cpu_timer++;
@@ -157,7 +265,8 @@ public class Simulation{
         }
 
         //simulateFCFS(processes);
-        simulateMultilevelQueues(processes, 5, 5);
+        //simulateMultilevelQueues(processes, 5, 5);
+        simulateFairShareScheduling(processes, 2, 5);
 
     }
 
